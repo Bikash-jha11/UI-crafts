39fd5ab4951e7b8d28555eec8e4c9478a7dbd5ad
Bikash <bikashjha490@gmail.com>
2025-11-12 20:50:00 +0545

    scheduler completed

--- src/component/Scheduler.tsx
+++ src/component/Scheduler.tsx
@@ -6,8 +6,11 @@ import isInside from '../logics/isInside';
 function Scheduler() {
     const ref = useRef(null);
     const moverRef = useRef(null);
+    const eventRef = useRef(null);
     const [data, setData] = useState({ mouseX: 0, mouseY: 0 });
-    const[isDraggind,setDragging] = useState(false);
+    const [draggedlength, setDraggedLength] = useState(0)
+    const [isDragging, setDragging] = useState(false);
+    const currLenRef = useRef(0);
     const { mouseX, mouseY } = data;
 
 
@@ -18,29 +21,51 @@ function Scheduler() {
             rect = ref.current.getBoundingClientRect();
         }
 
+        const handleMouseDown = (e) => {
+            currLenRef.current = e.clientY - rect.top;
+            setDragging(false);
+
+        }
+        const handleMouseUp = (e) => {
+            const nowAt = e.clientY - rect.top;
+            const start = currLenRef.current;
+            const cursordiff = Math.abs(nowAt - start)
+            console.log({ "diff is": start - nowAt })
+            if (isDragging && eventRef.current) {
+                eventRef.current.style.display = `inline-block`;
+                eventRef.current.style.top = `${mouseY - cursordiff}px`;
+                eventRef.current.style.height = `${cursordiff}px`;
+            }
+            setDragging(false);
+        }
         const handleMove = (e) => {
             const x = e.clientX - rect.left;
             const y = e.clientY - rect.top;
             setData({ mouseX: x, mouseY: y });
+            setDragging(true)
             if (rafid === null) {
                 rafid = window.requestAnimationFrame(() => {
                     if (rect && isInside(rect, e.clientX, e.clientY)) {
-                         moverRef.current.style.top = `${mouseY}px`;
+                        moverRef.current.style.top = `${mouseY}px`;
                     }
                     rafid = null;
                 })
             }
 
         }
-        const handleDrag = (e) => {
-             setDragging(true);
-        }
+
 
         window.addEventListener('mousemove', handleMove)
-        window.addEventListener('mousedown', handleDrag)
+        window.addEventListener('mousedown', handleMouseDown)
+        window.addEventListener('mouseup', handleMouseUp)
 
-        return () => window.removeEventListener('mousemove', handleMove);
-        return () => window.removeEventListener('mousedown', handleDrag);
+
+        return () => {
+            window.removeEventListener('mousemove', handleMove);
+            window.removeEventListener('mousedown', handleMouseDown)
+            window.removeEventListener('mouseup', handleMouseUp)
+        }
+
 
     }, [mouseX, mouseY])
     return (
@@ -63,7 +88,9 @@ function Scheduler() {
                     <div className={styles.mover} ref={moverRef}>
 
                     </div>
-                <div className={styles.events}></div>
+                    <div className={styles.events} ref={eventRef}>
+                        <p>Enter your schedule....</p>
+                    </div>
                 </div>
             </div>
         </div>
